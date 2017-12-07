7853ce685ecd2215a632696efef8644fad37d835
David Harris <dharr419@ford.com>
2017-12-06 20:41:34 -0500

    Day 6

---
+++ day6/6b.py
@@ -0,0 +1,20 @@
+from copy import copy
+
+with open('day6.txt', 'r') as f:
+    currentState = [int(line) for line in f.read().split()]
+
+    turns = 0
+    previousStates = []
+
+    while currentState not in previousStates:
+        previousStates.append(copy(currentState))
+        big = max(currentState)
+        index = currentState.index(big)
+        currentState[index] = 0
+        while big > 0:
+            index = (index+1)%len(currentState)
+            currentState[index] += 1
+            big -= 1
+        turns += 1
+        
+    print(len(previousStates) - previousStates.index(currentState))
