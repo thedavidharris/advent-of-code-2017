a126eabea5b601d14ef9a794f5477928e32d3a81
David Harris <dharr419@ford.com>
2017-12-08 20:01:56 -0500

    Code golfing 8a

--- day8/8a.py
+++ day8/8a.py
@@ -1,14 +1,10 @@
 from collections import defaultdict
 
-with open('day8.txt', 'r') as f:
-    input = f.readlines()
-    registers = defaultdict(int)
-    for line in input:
-        reg, instruction, number, condition, condReg, op, condNum = line.split()
-        if eval("registers[condReg] " + op + condNum):
-            if instruction == "inc":
-                registers[reg] += int(number)
-            else:
-                registers[reg] -= int(number)
+input = [line.strip("\n").replace("dec", "-=").replace("inc", "+=")  for line in open('day8.txt').readlines()]
 
-    print(max(registers.values()))
+registers = defaultdict(int)
+for line in input:
+    line += " else 0"
+    exec(line, {}, registers)
+
+print(max(registers.values()))
